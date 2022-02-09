a3a1a38cffa396bcc702b7715b1f206154ca4406
Synui <jonathon.schoenholz@yahoo.com>
2022-02-09 13:47:00 -0500

    user route updated, user model updated

---
+++ seeds/index.js
@@ -0,0 +1,19 @@
+const sequelize = require('../config/connection');
+
+const userSeeds = require('./user-seeds');
+const postSeeds = require('./post-seeds');
+const commentSeeds = require('./comment-seeds')
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await userSeeds();
+
+//   await postSeeds();
+
+//   await commentSeeds();
+
+  process.exit(0);
+};
+
+seedAll();
